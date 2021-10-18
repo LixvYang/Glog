8a514dc162706fbea6279760b2e3e9483d650f1c
lixin yang <2690688423@qq.com>
2021-10-18 16:39:48 +0800

    complete login

--- web/admin/src/plugin/antui.js
+++ web/admin/src/plugin/antui.js
@@ -1,4 +1,15 @@
 import Vue from 'vue'
-import { Button } from 'ant-design-vue'
+import { Button, FormModel, Input, Icon, message } from 'ant-design-vue'
+
+Vue.prototype.$message = message
+
+message.config({
+  top: '60px',
+  duration: 2,
+  maxCount: 3
+})
 
 Vue.use(Button)
+Vue.use(Input)
+Vue.use(FormModel)
+Vue.use(Icon)
